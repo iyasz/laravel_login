87a40919789107f3f691e9db20f36276d19a6fd4
Yasz Avellia <zakamaragames@gmail.com>
2022-12-03 19:06:18 +0700

    menambah tampilan

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container mt-5">
+        <div class="row mt-5 justify-content-center">
+            <div class="col-lg-5 mt-4">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <h1>Login</h1>
+                        <label for="">Username</label>
+                        <input type="text" name="username" class="form-control mb-3" id="">
+                        <label for="">Password</label>
+                        <input type="text" name="username" class="form-control mb-3" id="">
+                        <button class="btn btn-primary">Masuk</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
